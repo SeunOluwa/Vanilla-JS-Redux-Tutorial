ef7afdbebf5ec96b17120d320b7c2629ec4c880f
Taiwo Oluwaseun <taiwoluwaseun8@gmail.com>
2021-08-09 15:15:59 +0100

    redux store

--- index.js
+++ index.js
@@ -1,3 +1,7 @@
+// redux
+const redux = require('redux');
+const createStore = redux.createStore;
+
 // action type
 const BUY_CAKE = 'BUY_CAKE';
 
@@ -26,3 +30,12 @@ const reducer = (state = initialState, action) => {
                 return state;
     };
 };
+
+// redux store
+const store = createStore(reducer);
+console.log('Initial state', store.getState());
+const unsubscribe = store.subscribe(() => console.log('Updated state', store.getState()));
+store.dispatch(buyCake())
+store.dispatch(buyCake())
+store.dispatch(buyCake())
+unsubscribe();
